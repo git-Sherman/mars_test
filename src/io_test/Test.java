0c776ca4d8a25be3325d1c15ac03d6dc0eee25ed
git-Sherman <linshengmiao@hotmail.com>
2015-12-16 23:07:28 +0800

    学习了java I/O（一） 	fileinputstream类 中的      int read（存放读入数据的数组，偏移位置，读入的长度）--》有返回值int，值为读入的个数 int read(byte[] b,int off,int len); 	fileoutputstream类中的     void write（输出数据的数组，偏移位置，输出长度） 						  void write(byte[] b,int off,int len);

---
+++ src/io_test/Test.java
@@ -0,0 +1,35 @@
+package io_test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		//声明输入流引用
+		FileInputStream fis=null;
+		//声明输出流引用
+		
+		FileOutputStream fos=null;
+		try{
+			//生成代表输入流的对象
+			fis=new FileInputStream("D:/from.txt");
+			//生成代表输出流的对象
+			fos=new FileOutputStream("D:/to.txt");
+			//生成一个字节数组
+			byte[] butter=new byte[100];
+//			//调用输入流的对象的read方法，读取数据
+			int temp=fis.read(butter,0,butter.length);
+			fos.write(butter,0,temp);
+//			String s=new String(butter);
+//			//调用一个String对象的trim方法，可以去除掉这个字符串的首尾空格和空字符
+//			s=s.trim();
+//			System.out.println(s);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
